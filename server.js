c660179eacf4d57f606ebc98af28903ba978db8b
Denis Ouma <dennisouma38@gmail.com>
2023-06-03 02:02:29 +0300

    Setting up the API for the application

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const color = require("color");
+// loading the dotenv file
+dotenv.config({ path: "./config/config.env" });
+
+// Load the routes
+const products = require("./routes/productRoute");
+const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+// Moute routers
+app.use("/api/v1/products", products);
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+
+	// cloese Server and exit process
+	server.close(() => process.exit(1));
+});
